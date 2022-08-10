ed2817e2f968a1c2302bac0cac56ac6e47b4e87e
Edson <j.edson.da.silva.s@gmail.com>
2022-08-10 15:54:11 -0300

    componentes e button

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,17 @@
 import { Container } from './styles'
 
+import { Button } from '../../components/Button'
+
 export function Details(){
   
   return (
     <Container>
       <h1>Hello word</h1>
       <h2>1542df</h2>
+
+      <Button title="entrar" loading/>
+      <Button title="cadastyrar"/>
+
     </Container>
   )
 }

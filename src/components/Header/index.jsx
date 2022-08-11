2f20f820cf4943c3381ea3e093f1aefe9ba22d2c
Edson <j.edson.da.silva.s@gmail.com>
2022-08-11 09:29:12 -0300

    criando componetes e ajustando cabecario

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,21 @@
-import { Container } from "./styles";
+import { Container, Profile, Logout} from "./styles";
+import { RiShutDownLine} from 'react-icons/ri'
 
 export function Header(){
     return(
         <Container>
+            <Profile>
+                <img src="https://github.com/edson670.png" alt="foto do usuario"/>
+                
+                <div>
+                    <span>Bem-vindo</span>
+                    <strong>Edson Silva</strong>
+                </div>
+            </Profile>
             
+            <Logout>
+                <RiShutDownLine/>
+            </Logout>
         </Container>
     )
 }

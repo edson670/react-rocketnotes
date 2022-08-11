c21ecdcb5f0a53e635b5df88fa59996538aeafcb
Edson <j.edson.da.silva.s@gmail.com>
2022-08-11 17:32:52 -0300

    criando links e componetes de butões

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,7 +1,11 @@
-import { Container } from './styles'
+import { Container, Links } from './styles'
 
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
+import { Section } from '../../components/Section'
+import { Tag } from '../../components/Tag'
+
+
 
 
 export function Details(){
@@ -9,6 +13,21 @@ export function Details(){
   return (
     <Container>
       <Header/>
+
+      <Section title="Links Ùteis">
+        <Links>
+          <li> <a href="#">https://www.rocketset.com.br/</a> </li>
+          <li> <a href="#">https://www.rocketset.com.br/</a> </li>
+          
+        </Links>  
+      </Section>
+
+      <Section title="Marcadores">
+
+        <Tag title="express" /> 
+        <Tag title="nodejs" />
+      
+      </Section>
 
       <Button title="Voltar"/>
 

e499a47517c8506dc7638f03f29cb4e91d1d72e8
Edson <j.edson.da.silva.s@gmail.com>
2022-08-15 15:41:13 -0300

    finalizando a pagina home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,13 @@
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
+import {FiPlus, FiSearch} from 'react-icons/fi'
 
 import { Header } from '../../components/Header'
+import { Input } from '../../components/Input'
+import { Section } from '../../components/Section'
+import { ButtonText } from '../../components/ButtonText'
+import { Note } from '../../components/Note'
+
+
 
 export function Home(){
     return(
@@ -12,19 +19,33 @@ export function Home(){
             <Header/>
 
             <Menu>
-
+                <li><ButtonText title="Todos" isActive/></li>
+                <li><ButtonText title="Todos"/></li>
+                <li><ButtonText title="Todos"/></li>
             </Menu>
 
             <Search>
-
+                <Input placeholder="Pesquisar pelo titulo" icon={FiSearch}/>
             </Search>
 
             <Content>
-
+                <Section title="Minhas notas">
+                    <Note data={{ 
+                        title: 'React',
+                        tags:[
+                            {id: '1', name:'react'},
+                            {id: '2', name:'rocketseet'}
+                            ]
+                        }}
+                        />
+                        
+                        
+                </Section>
             </Content>
 
             <NewNote>
-
+                <FiPlus />
+                Criar nota
             </NewNote>
         </Container>
     )

fe1ba2fdd4337e31c692193c110855331e7235ca
Edson <j.edson.da.silva.s@gmail.com>
2022-08-17 15:16:20 -0300

    finalizando interface de notas

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,23 +1,39 @@
 import { Header } from '../../components/Header'
 import { Input } from '../../components/Input'
 import { Textarea } from '../../components/Textarea'
+import { NoteItem } from '../../components/NoteItem'
+import { Section } from '../../components/Section'
+import { Button } from '../../components/Button'
 
 
 
 import { Container, Form } from './styles'
 
-export function New(){
-    return(
+export function New() {
+    return (
         <Container>
-            <Header/>
+            <Header />
             <main>
                 <Form>
                     <header>
                         <h1>Criar nota</h1>
                         <a href='/'>Voltar</a>
                     </header>
-                    <Input  placeholder="Titulo"/>
-                    <Textarea placeholder="Observaçôes"/>
+                    <Input placeholder="Titulo" />
+                    <Textarea placeholder="Observaçôes" />
+
+                    <Section title="Links Ùteis">
+                        <NoteItem value="https://rocketseat.com.br" />
+                        <NoteItem isNew placeholder="Novo Link" />
+                    </Section>
+
+                    <Section title="Marcadores">
+                        <div className="tags">
+                            <NoteItem value="react" />
+                            <NoteItem isNew placeholder="Novo tag" />
+                        </div>
+                    </Section>
+                    <Button title="Salvar"/>
                 </Form>
             </main>
         </Container>

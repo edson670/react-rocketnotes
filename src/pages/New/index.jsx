3ade2c6e3cd70ca10eb4eca5fd2dfcbae4b949de
Edson <j.edson.da.silva.s@gmail.com>
2022-08-25 16:38:50 -0300

    removendo links

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -22,6 +22,9 @@ export function New() {
         setNewLink("")
     }
 
+    function handleRemoveLink(deleted){
+        setLinks(prevState => prevState.filter(link => link !== deleted))
+    }
 
 
     return (
@@ -42,7 +45,7 @@ export function New() {
                                 <NoteItem  
                                     key={String(index)}
                                     value = { link }
-                                    onClick={() => { }}
+                                    onClick={() => handleRemoveLink(link)}
                                 />    
                             ))
 
